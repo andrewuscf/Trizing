3579633779e4417420f41418372036a0ebd9e9a3
Andrew <andrewuscf@hotmail.com>
2017-03-21 00:47:54 -0700

    Working on chat.

--- app/components/ChatRoomBox.js
+++ app/components/ChatRoomBox.js
@@ -14,10 +14,10 @@ import AvatarImage from './AvatarImage';
 moment.updateLocale('en', {
     relativeTime: {
         mm: "%dm",
-        h:  "1h",
+        h: "1h",
         hh: "%dh",
-        s:  "%ds",
-        d:  "1d",
+        s: "%ds",
+        d: "1d",
         dd: "%dd",
     }
 });
@@ -26,6 +26,7 @@ const ChatRoomBox = React.createClass({
     propTypes: {
         room: React.PropTypes.object.isRequired,
         _redirect: React.PropTypes.func.isRequired,
+        RequestUser: React.PropTypes.object.isRequired,
     },
 
     _toRoom() {
@@ -40,27 +41,27 @@ const ChatRoomBox = React.createClass({
 
     render() {
         const room = this.props.room;
-        console.log(room)
-        // return (
-        //     <TouchableHighlight style={styles.container} onPress={this._toRoom} underlayColor='white'>
-        //         <View style={styles.inner}>
-        //             <AvatarImage image={sender.profile.avatar}/>
-        //             <View style={styles.details}>
-        //                 <Text style={styles.name}>{sender.first_name} {sender.last_name[0]}.</Text>
-        //                 <Text style={styles.small}>
-        //                     6 miles away
-        //                 </Text>
-        //                 <View style={styles.lastMessageSection}>
-        //                     <Text style={styles.bold}>{this.trimToLength(room.last_message.message, 25)}</Text>
-        //                     {(room.last_message.message) ?
-        //                         <Text style={styles.timeAgo}>{moment(room.last_message.timestamp).fromNow(false)}</Text>
-        //                         : null
-        //                     }
-        //                 </View>
-        //             </View>
-        //         </View>
-        //     </TouchableHighlight>
-        // )
+        let sender = room.users[room.users.length - 1];
+        if (this.props.RequestUser.id == sender.id)
+            sender = room.users[room.users.length - 2];
+        let image = sender.profile.thumbnail ? sender.profile.thumbnail : sender.profile.avatar
+        return (
+            <TouchableHighlight style={styles.container} onPress={this._toRoom} underlayColor='white'>
+                <View style={styles.inner}>
+                    <AvatarImage image={sender.profile.avatar}/>
+                    <View style={styles.details}>
+                        <Text style={styles.name}>{sender.profile.first_name} {sender.profile.last_name[0]}.</Text>
+                        {(room.last_message && room.last_message.message) ?
+                            <View style={styles.lastMessageSection}>
+                                <Text style={styles.bold}>{this.trimToLength(room.last_message.message, 25)}</Text>
+                                <Text style={styles.timeAgo}>{moment(room.last_message.timestamp).fromNow(false)}</Text>
+
+                            </View> : null
+                        }
+                    </View>
+                </View>
+            </TouchableHighlight>
+        )
     }
 });
 
@@ -69,7 +70,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         borderBottomWidth: .5,
-        borderBottomColor: 'rgba(0,0,0,.15)'
+        borderBottomColor: 'rgba(0,0,0,.15)',
+        marginTop:10,
+        backgroundColor: 'white'
     },
     inner: {
         flex: 1,
