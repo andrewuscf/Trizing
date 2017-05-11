d8bcc98f99d004e660a3d9f704c59e591cd150db
Andrew <andrewuscf@hotmail.com>
2017-05-11 15:29:01 -0700

    Back to development

--- ios/Trizing/AppDelegate.m
+++ ios/Trizing/AppDelegate.m
@@ -27,9 +27,9 @@
                            didFinishLaunchingWithOptions:launchOptions];
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
-  // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Trizing"
