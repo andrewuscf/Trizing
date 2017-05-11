5ae02d31982f2a46698885a515f95fe4759cfca1
Andrew <andrewuscf@hotmail.com>
2017-05-11 15:28:43 -0700

    Splash art

--- ios/Trizing/AppDelegate.m
+++ ios/Trizing/AppDelegate.m
@@ -27,7 +27,9 @@
                            didFinishLaunchingWithOptions:launchOptions];
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+
+  // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Trizing"
