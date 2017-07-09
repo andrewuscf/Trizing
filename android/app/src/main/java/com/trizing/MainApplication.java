3cf1faae5cf007b954a10667b849f4a84471caf4
Andrew <andrewuscf@hotmail.com>
2017-07-08 22:26:21 -0700

    Fixed issue with custom icons.

--- android/app/src/main/java/com/trizing/MainApplication.java
+++ android/app/src/main/java/com/trizing/MainApplication.java
@@ -42,7 +42,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-            new FBSDKPackage(),
             new RNFetchBlobPackage(),
             new PickerPackage(),
             new FIRMessagingPackage(),
