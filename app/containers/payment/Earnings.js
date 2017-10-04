ba4c7581b301bfb4afc39670df965e588597c584
andrewuscf <andrewuscf@hotmail.com>
2017-10-04 09:24:08 -0700

    Charges setup.

--- app/containers/payment/Earnings.js
+++ app/containers/payment/Earnings.js
@@ -13,6 +13,7 @@ import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import _ from 'lodash';
 import RNFetchBlob from 'react-native-fetch-blob';
 
+import AvatarImage from '../../components/AvatarImage';
 import GlobalStyle from '../../containers/globalStyle';
 import {API_ENDPOINT, checkStatus, getFontSize, setHeaders} from '../../actions/utils';
 
@@ -43,6 +44,7 @@ const Earnings = React.createClass({
                 this.setState({payoutInfo: responseJson});
             }).catch((errorMessage) => {
             console.log(errorMessage.status)
+            this.setState({payoutInfo: {}});
         });
     },
 
@@ -79,8 +81,8 @@ const Earnings = React.createClass({
     convertToMap() {
         const eventMap = {}; // Create the blank map
         this.state.charges.forEach((charge) => {
-            if (!this.state.searchText || _.includes(charge.name.toLowerCase(), this.state.searchText.toLowerCase())) {
-                const firstLetter = charge.name[0];
+            if (!this.state.searchText || _.includes(charge.customer.name.toLowerCase(), this.state.searchText.toLowerCase())) {
+                const firstLetter = charge.customer.name[0];
                 if (!eventMap[firstLetter]) {
                     eventMap[firstLetter] = [];
                 }
@@ -100,57 +102,11 @@ const Earnings = React.createClass({
     },
 
     renderHeader() {
-        if (!this.state.payoutInfo) return null;
-        let charges_total = 0;
-        let total_regs = 0;
-        this.state.charges.forEach((charge) => {
-            if (charge.charges_total) charges_total += charge.charges_total;
-            if (charge.charges_count) total_regs += charge.charges_count;
-        });
-        charges_total = charges_total.toFixed(2);
-        let payTo = null;
-        if (this.state.payoutInfo) {
-            if (this.state.payoutInfo.business_name && this.state.payoutInfo.account_type === 'company') {
-                payTo = this.state.payoutInfo.business_name
-            } else if (this.state.payoutInfo.bank_account &&
-                this.state.payoutInfo.bank_account.account_holder_name &&
-                this.state.payoutInfo.account_type === 'individual') {
-                payTo = this.state.payoutInfo.bank_account.account_holder_name;
-            }
-        }
-        const {navigate} = this.props.navigation;
         return (
             <View>
-                <View style={[styles.topSection, {
-                    marginBottom: 0,
-                    borderBottomLeftRadius: 0,
-                    borderBottomRightRadius: 0
-                }]}>
-                    <Text style={[styles.totalText, styles.greenText]}>${charges_total}</Text>
-                    <Text style={[styles.totalReg]}>{total_regs} Registrations</Text>
-                </View>
-                <View style={[styles.topSection, {marginTop: 0, borderTopLeftRadius: 0, borderTopRightRadius: 0}]}>
-                    <Text style={[styles.totalText, styles.greenText]} onPress={() => {
-                        if (this.state.payoutInfo) {
-                            navigate('GroupPayouts', {
-                                payoutInfo: this.state.payoutInfo,
-                                groupId: this.props.groupId,
-                                onPaymentInfoUpdate: this.onPaymentInfoUpdate
-                            })
-                        }
-                    }}>
-                        {this.state.payoutInfo.payout_total ? `$${this.state.payoutInfo.payout_total}` : '$0.00'}
-                    </Text>
-                    <Text style={[styles.totalReg]} onPress={() => {
-                        if (this.state.payoutInfo) {
-                            navigate('Earnings', {
-                                payoutInfo: this.state.payoutInfo,
-                                onPaymentInfoUpdate: this.onPaymentInfoUpdate
-                            })
-                        }
-                    }}>
-                        Paid out to: {payTo}
-                    </Text>
+                <View style={[styles.topSection, {borderTopLeftRadius: 10, borderTopRightRadius: 10, marginBottom: 0}]}>
+                    <Text style={[styles.totalText, styles.greenText]}>1</Text>
+                    <Text style={[styles.totalReg]}>1 Registrations</Text>
                 </View>
                 <View style={[styles.subNav]}>
                     <MaterialIcon name="search" size={20} color='#797979'/>
@@ -176,20 +132,32 @@ const Earnings = React.createClass({
     },
 
     renderRow(charge, sectionId, rowId) {
+        let refunded = charge.refunded || charge.disputed;
         return (
-            <View style={[styles.connectionBox,
+            <View activeOpacity={1} style={[styles.connectionBox,
                 rowId == this.state.charges.length - 1 ? {
                     borderBottomRightRadius: 10,
                     borderBottomLeftRadius: 10
                 } : null]}>
-                <View style={{flex: .9, flexDirection: 'row', alignItems: 'center'}}>
-                    <Text style={styles.name}>{charge.name}</Text>
+                <View style={[{flex: .9, flexDirection: 'row', alignItems: 'center'}, refunded ? {opacity: .4} : null]}>
+                    <AvatarImage image={charge.customer.image} style={styles.avatar} cache={true}/>
+                    <Text style={styles.name}>{charge.customer.name}</Text>
                 </View>
-                <Text style={[styles.name, styles.greenText]}>
-                    {charge.charges_total ? `$${charge.charges_total.toFixed(2)}` : '$0.00'}
-                </Text>
+                {refunded ?
+                    <Text style={[styles.name, {color: 'red'}]}>
+                        -{charge.amount_refunded ? `$${parseFloat(charge.amount_refunded).toFixed(2)}` : '$0.00'}
+                    </Text> :
+                    <Text style={[styles.name, styles.greenText]}>
+                        {charge.amount ? `$${parseFloat(charge.amount).toFixed(2)}` : '$0.00'}
+                    </Text>
+                }
             </View>
         )
+    },
+
+    refresh() {
+        this.getReceipts(true);
+        this.getPaymentInfo();
     },
 
 
@@ -200,19 +168,17 @@ const Earnings = React.createClass({
         });
         let dataSource = ds.cloneWithRowsAndSections(this.convertToMap());
         return (
-            <View style={GlobalStyle.container}>
-                {/*<ListView removeClippedSubviews={(Platform.OS !== 'ios')}*/}
-                {/*showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}*/}
-                {/*style={[GlobalStyle.container]}*/}
-                {/*refreshControl={<RefreshControl refreshing={this.state.refresh}*/}
-                {/*onRefresh={() => this.getReceipts(true)}/>}*/}
-                {/*enableEmptySections={true}*/}
-                {/*renderHeader={this.renderHeader}*/}
-                {/*renderSectionHeader={this.renderSectionHeader}*/}
-                {/*dataSource={dataSource}*/}
-                {/*renderRow={this.renderRow}*/}
-                {/*/>*/}
-            </View>
+            <ListView removeClippedSubviews={(Platform.OS !== 'ios')}
+                      showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}
+                      style={[GlobalStyle.container]}
+                      refreshControl={<RefreshControl refreshing={this.state.refresh}
+                                                      onRefresh={this.refresh}/>}
+                      enableEmptySections={true}
+                      renderHeader={this.renderHeader}
+                      renderSectionHeader={this.renderSectionHeader}
+                      dataSource={dataSource}
+                      renderRow={this.renderRow}
+            />
         );
     }
 });
@@ -222,8 +188,8 @@ const styles = StyleSheet.create({
         flex: 1,
         width: 105,
         color: '#797979',
-        fontSize: 14,
-        fontFamily: 'Gotham-Medium',
+        fontSize: getFontSize(14),
+        fontFamily: 'Heebo-Medium',
         borderWidth: 0,
         backgroundColor: 'transparent',
         paddingLeft: 5,
@@ -251,7 +217,7 @@ const styles = StyleSheet.create({
     },
     sectionTitle: {
         fontSize: getFontSize(18),
-        fontFamily: 'Gotham-Medium',
+        fontFamily: 'Heebo-Medium',
         color: '#00BD89',
         paddingLeft: 10,
         paddingTop: 10,
@@ -275,87 +241,20 @@ const styles = StyleSheet.create({
         marginRight: 10
     },
     name: {
-        fontFamily: 'Gotham-Medium',
+        fontFamily: 'Heebo-Medium',
         fontSize: getFontSize(20),
     },
     greenText: {
         color: '#00BD89',
     },
-    innerModal: {
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderTopLeftRadius: 4,
-        borderTopRightRadius: 4,
-        borderColor: 'rgba(0, 0, 0, 0.1)',
-    },
-    closeButton: {
-        backgroundColor: 'white',
-        padding: 12,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderBottomLeftRadius: 4,
-        borderBottomRightRadius: 4,
-        borderTopWidth: .5,
-        borderColor: 'rgba(0, 0, 0, 0.1)',
-    },
-    modalDetail: {
-        fontSize: getFontSize(22),
-        fontFamily: 'Gotham-Medium',
-        color: 'grey',
-        paddingTop: 20,
-        paddingBottom: 20
-    },
-    chargeDescription: {
-        padding: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexWrap: 'wrap'
-    },
-    chargeText: {
-        fontSize: getFontSize(18),
-        fontFamily: 'Gotham-Medium',
-    },
     totalText: {
         fontSize: getFontSize(72),
-        fontFamily: 'Gotham-Medium',
+        fontFamily: 'Heebo-Medium',
     },
     totalReg: {
         fontSize: getFontSize(20),
-        fontFamily: 'Gotham-Medium',
+        fontFamily: 'Heebo-Medium',
         color: 'grey'
-    },
-    buttonSection: {
-        flexDirection: 'row'
-    },
-    modalButton: {
-        flex: .5,
-        marginTop: 0,
-        marginLeft: 10,
-        marginRight: 10,
-        height: 40,
-        backgroundColor: 'transparent',
-        borderColor: '#00BD89',
-        borderWidth: 1,
-        marginBottom: 30,
-        borderRadius: 20
-    },
-    modalButtonText: {
-        color: '#00BD89',
-        fontSize: getFontSize(12),
-        fontFamily: 'Gotham-Medium'
-    },
-    modalButtonGreen: {
-        marginTop: 0,
-        height: 40,
-        width: getFontSize(250),
-        marginBottom: 30,
-        borderRadius: 20
-    },
-    clearButton: {
-        backgroundColor: 'transparent',
-        borderColor: '#00BD89',
-        borderWidth: 1,
     }
 });
 
